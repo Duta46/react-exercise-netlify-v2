28682557dd1466333dbd417a2659319e4c07ba09
Duta46 <dutaalif.20091@mhs.unesa.ac.id>
2023-06-17 19:08:06 +0700

    First commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Duta Alif Gunawan" // TODO: replace this
+const studentId = "FE5379917" // TODO: replace this
+
+export { url, studentName, studentId }
